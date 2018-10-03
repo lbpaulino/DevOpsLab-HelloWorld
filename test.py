448785e84442437d569393b38d1af3f36a763d91
lbpaulino <lbpaulino@yahoo.com.br>
2018-10-03 13:25:21 -0400

    update IV

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps Essentials - Hello World War III !")
+        self.assertRegex(result.data.decode(), "DevOps Essentials - Hello World War IV !")
 
 
 if __name__ == "__main__":
